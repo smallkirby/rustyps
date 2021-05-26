0e3ad08e2fe5c13c5cfa7add25b18fec1947e8d0
smallkirby <ssmallkirby@gmail.com>
2021-05-26 18:31:05 +0900

    impl simple parser for GNU pid

--- src/argparser.rs
+++ src/argparser.rs
@@ -1,3 +1,10 @@
+use std::collections::LinkedList;
+
+// globals
+
+// end globals
+
+
 #[derive(Debug, PartialEq)]
 pub enum ArgType {
     ArgGnu,
@@ -7,6 +14,152 @@ pub enum ArgType {
     ArgBsd,
     ArgFail,
     ArgSess,
+}
+
+
+#[derive(Debug, PartialEq)]
+pub struct SelectionNode {
+  u: Box<SelUnion>,
+  n: i32,                   // number of items
+  typecode: SelectionListType,
+}
+
+#[derive(Debug, PartialEq)]
+pub enum SelectionListType {
+  SelrUid,
+  SeleUid,
+  SelsUid,
+  SelfUid,
+  SelrGid,
+  SeleGid,
+  SelsGid,
+  SelfGid,
+  SelpGrp,
+  SelPid ,
+  SelTty ,
+  SelSess,
+  SelComm,
+  SelPpid,
+  SelPidQuick,
+}
+
+#[derive(Debug, PartialEq)]
+pub struct SelUnion {
+  val: u64,
+}
+
+pub enum GnuTable {
+  Group,
+  User,
+  Cols,
+  Columns,
+  Context,
+  Comulative,
+  Deselect,
+  Forest,
+  Format,
+  Sgroup, // group
+  Header,
+  Headers,
+  Heading,
+  Headings,
+  Info,
+  Lines,
+  NoHeader,
+  NoHeaders,
+  Noheader,
+  Noheaders,
+  NoHeading,
+  NoHeadings,
+  Noheading,
+  Noheadings,
+  Pid,
+  Ppid,
+  QuickPid,
+  Rows,
+  Sid,
+  Sort,
+  Tty,
+  Version,
+  Width,
+}
+
+impl GnuTable {
+  pub fn from_str(s: &str) -> Option<GnuTable> {
+    match s {
+      "Group" => Some(GnuTable::Group), 
+      "User" => Some(GnuTable::User),
+      "cols" => Some(GnuTable::Cols), 
+      "columns" => Some(GnuTable::Columns), 
+      "context" => Some(GnuTable::Context), 
+      "comulative" => Some(GnuTable::Comulative), 
+      "deselect" => Some(GnuTable::Deselect), 
+      "forest"=> Some(GnuTable::Forest), 
+      "format" => Some(GnuTable::Format), 
+      "group" => Some(GnuTable::Sgroup), 
+      "header" => Some(GnuTable::Header), 
+      "headers" => Some(GnuTable::Headers), 
+      "heading" => Some(GnuTable::Heading), 
+      "headings" => Some(GnuTable::Headings), 
+      "info" => Some(GnuTable::Info), 
+      "lines" => Some(GnuTable::Lines), 
+      "no-header" => Some(GnuTable::NoHeader), 
+      "no-headers" => Some(GnuTable::NoHeaders), 
+      "noheader" => Some(GnuTable::Noheader), 
+      "noheaders" => Some(GnuTable::Noheaders), 
+      "no-heading" => Some(GnuTable::NoHeading), 
+      "no-headings" => Some(GnuTable::NoHeadings), 
+      "noheading" => Some(GnuTable::Noheading), 
+      "noheadings" => Some(GnuTable::Noheadings), 
+      "pid" => Some(GnuTable::Pid), 
+      "ppid" => Some(GnuTable::Ppid), 
+      "quick-pid" => Some(GnuTable::QuickPid), 
+      "rows" => Some(GnuTable::Rows), 
+      "sid" => Some(GnuTable::Sid), 
+      "sort" => Some(GnuTable::Sort), 
+      "tty" => Some(GnuTable::Tty), 
+      "version" => Some(GnuTable::Version), 
+      "width" => Some(GnuTable::Width), 
+      _ => None,
+    }
+  }
+  pub fn as_str(&self) -> &'static str {
+    match *self {
+      GnuTable::Group => "Group",
+      GnuTable::User => "user",
+      GnuTable::Cols => "cols",
+      GnuTable::Columns => "colmns",
+      GnuTable::Context => "context",
+      GnuTable::Comulative => "comulative",
+      GnuTable::Deselect => "deselect",
+      GnuTable::Forest => "forest",
+      GnuTable::Format => "format",
+      GnuTable::Sgroup => "group",
+      GnuTable::Header => "header",
+      GnuTable::Headers => "headers",
+      GnuTable::Heading => "heading",
+      GnuTable::Headings =>"headings",
+      GnuTable::Info => "info",
+      GnuTable::Lines => "lines",
+      GnuTable::NoHeader => "no-header",
+      GnuTable::NoHeaders => "no-headers",
+      GnuTable::Noheader => "noheader",
+      GnuTable::Noheaders => "noheaders",
+      GnuTable::NoHeading => "no-heading",
+      GnuTable::NoHeadings => "no-headings",
+      GnuTable::Noheading => "noheading",
+      GnuTable::Noheadings => "noheadings",
+      GnuTable::Pid => "pid",
+      GnuTable::Ppid => "ppid",
+      GnuTable::QuickPid => "quick-pid",
+      GnuTable::Rows => "rows",
+      GnuTable::Sid => "sid",
+      GnuTable::Sort => "sort",
+      GnuTable::Tty => "tty",
+      GnuTable::Version => "version",
+      GnuTable::Width => "width",
+    }
+  } 
 }
 
 pub fn arg_type(arg: &String) -> ArgType {
@@ -46,6 +199,91 @@ pub fn arg_type(arg: &String) -> ArgType {
     }
 }
 
+pub fn arg_parse(args: Vec<String>) -> Result<LinkedList<Box<SelectionNode>>, String> {
+  let mut selection_list = LinkedList::new();
+  match parse_all_options(&args) {
+    Ok(list) => {
+      let mut tmp = list;
+      selection_list.append(&mut tmp);
+    }
+    Err(msg) => return Err(msg),
+  };
+
+  Ok(selection_list)
+}
+
+
+pub fn parse_all_options(args: &Vec<String>) -> Result<LinkedList<Box<SelectionNode>>, String> {
+  let mut selection_list = LinkedList::new();
+  for (ix,arg) in args.iter().enumerate() {
+      log::trace!("argv[{}] is {}", ix, arg);
+      if ix == 0 {
+          log::trace!("skip this arg");
+          continue;
+      }
+      match arg_type(&arg) {
+        ArgType::ArgBsd => {
+          log::trace!("{}: type ArgBsd", arg);
+        },
+        ArgType::ArgSysv => {
+          log::trace!("{}: type ArgSysv", arg);
+        },
+        ArgType::ArgGnu => {
+          log::trace!("{}: type ArgGnu", arg);
+          match parse_gnu_option(arg) {
+            Ok(list) => {
+              let mut tmp = list;
+              selection_list.append(&mut tmp);
+              log::trace!("parse_all_options: {:?}", selection_list);
+            },
+            Err(msg) => return Err(msg),
+          }
+        },
+        ArgType::ArgPgrp | ArgType::ArgSess | ArgType::ArgPid => {
+          log::trace!("{}: type Arg{{Pgrp|Sess|Pid}}", arg);
+        },
+        ArgType::ArgFail => {
+          log::trace!("{}: type ArgFail", arg);
+          return Err(String::from("ArgFail"));
+        },
+      }
+  }
+  Ok(selection_list)
+}
+
+pub fn parse_gnu_option(arg: &String) -> Result<LinkedList<Box<SelectionNode>>, String> {
+  log::trace!("parse_gnu_option()");
+
+  let mut selection_list = LinkedList::new();
+  let argpair = arg.split("=").collect::<Vec<&str>>(); // XXX don't support ":" separator for now.
+  if argpair.len() != 2 {
+    return Err(String::from("unknown gnu long option"));
+  }
+  let argname = match GnuTable::from_str(&argpair[0][2..]) {
+    Some(a) => a,
+    None => return Err(String::from("unknown gnu long option")),
+  };
+  let val = argpair[1];
+  match argname {
+    GnuTable::Pid => { // XXX now support only "--pid=xx" style
+      log::trace!("--pid processing");
+      let pid = match val.parse() {
+        Ok(p) => p,
+        Err(_) => return Err(String::from("cannot parse given PID")),
+      };
+      selection_list.push_front(Box::new(SelectionNode {
+        n: 1,
+        u: Box::new(SelUnion{val: pid}),
+        typecode: SelectionListType::SelPid,
+      }));
+    },
+    _ => unimplemented!(),
+  };
+
+  log::trace!("{:?}", selection_list);
+  return Ok(selection_list);
+}
+
 #[cfg(test)]
 mod tests {
   #[test]
@@ -65,5 +303,45 @@ mod tests {
     assert_eq!(super::arg_type(&String::from("---")), super::ArgType::ArgFail);
     assert_eq!(super::arg_type(&String::from("--")), super::ArgType::ArgFail);
     assert_eq!(super::arg_type(&String::from("--3")), super::ArgType::ArgFail);
+  }
+
+  #[test]
+  fn parse_gnu_option_pid_1() {
+    let args = vec![String::from("me"), String::from("--pid=39")];
+    let b0 = Box::new(super::SelectionNode{
+      u: Box::new(super::SelUnion{val: 39}),
+      n: 1,
+      typecode: super::SelectionListType::SelPid,
+    });
+    match super::arg_parse(args) {
+      Ok(list) => {
+        let mut tmp = list;
+        assert_eq!(tmp.pop_front().unwrap(), b0);
+      },
+      Err(msg) => panic!(msg),
+    }
+  }
+
+  #[test]
+  fn parse_gnu_option_pid_2() {
+    let args = vec![String::from("me"), String::from("--pid=39"), String::from("--pid=123")];
+    let b0 = Box::new(super::SelectionNode{
+      u: Box::new(super::SelUnion{val: 39}),
+      n: 1,
+      typecode: super::SelectionListType::SelPid,
+    });
+    let b1 = Box::new(super::SelectionNode{
+      u: Box::new(super::SelUnion{val: 123}),
+      n: 1,
+      typecode: super::SelectionListType::SelPid,
+    });
+    match super::arg_parse(args) {
+      Ok(list) => {
+        let mut tmp = list;
+        assert_eq!(tmp.pop_front().unwrap(), b0);
+        assert_eq!(tmp.pop_front().unwrap(), b1);
+      },
+      Err(msg) => panic!(msg),
+    }
   }
 }
--- src/main.rs
+++ src/main.rs
@@ -14,38 +14,7 @@ fn main() {
 
     // XXX must set sighandlers 
     /* */
-    arg_parse();
-}
-
-pub fn arg_parse() -> u64 {
-    use argparser::ArgType;
-
-    for (ix,arg) in env::args().enumerate() {
-        log::trace!("argv[{}] is {}", ix, arg);
-        if ix == 0 {
-            log::trace!("skip this arg");
-            continue;
-        }
-        match argparser::arg_type(&arg) {
-          ArgType::ArgBsd => {
-            log::trace!("{}: type ArgBsd", arg);
-          },
-          ArgType::ArgSysv => {
-            log::trace!("{}: type ArgSysv", arg);
-          },
-          ArgType::ArgGnu => {
-            log::trace!("{}: type ArgGnu", arg);
-          },
-          ArgType::ArgPgrp | ArgType::ArgSess | ArgType::ArgPid => {
-            log::trace!("{}: type Arg{{Pgrp|Sess|Pid}}", arg);
-          },
-          ArgType::ArgFail => {
-            log::trace!("{}: type ArgFail", arg);
-            do_help(String::from(""), -1,);
-          },
-        }
-    }
-    0
+    argparser::arg_parse(env::args().collect());
 }
 
 pub fn do_help(opt: String, rc: i32) {
