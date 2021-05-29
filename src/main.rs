c43efa217533a1d53b2085cf1182d558816bf82a
smallkirby <ssmallkirby@gmail.com>
2021-05-29 17:21:07 +0900

    use 2spaces

--- src/main.rs
+++ src/main.rs
@@ -4,39 +4,39 @@ use simple_logger::SimpleLogger;
 
 #[derive(Debug)]
 struct Ps {
-    parser: argparser::PsParser,
+  parser: argparser::PsParser,
 }
 
 impl Ps {
-    fn new() -> Ps {
-        Ps {
-            parser: argparser::PsParser::from(std::env::args()),
-        }
+  fn new() -> Ps {
+    Ps {
+      parser: argparser::PsParser::from(std::env::args()),
     }
-    fn run(self) -> i32 {
-        0
-    }
+  }
+  fn run(self) -> i32 {
+    0
+  }
 }
 
 fn main() {
-    SimpleLogger::new()
-        .with_level(log::LevelFilter::Trace)
-        .init()
-        .unwrap();
+  SimpleLogger::new()
+    .with_level(log::LevelFilter::Trace)
+    .init()
+    .unwrap();
 
-    // XXX have to register atexit func?
-    /* */
+  // XXX have to register atexit func?
+  /* */
 
-    let myname = std::env::current_exe();
-    //log::trace!("{:?}", myname);
+  let myname = std::env::current_exe();
+  //log::trace!("{:?}", myname);
 
-    // XXX must set sighandlers
-    /* */
-    let ps = Ps::new();
-    ps.run();
+  // XXX must set sighandlers
+  /* */
+  let ps = Ps::new();
+  ps.run();
 }
 
 pub fn do_help(opt: String, rc: i32) {
-    log::trace!("do_help called with opt:{}, rc:{}", opt, rc);
-    std::process::exit(rc);
+  log::trace!("do_help called with opt:{}, rc:{}", opt, rc);
+  std::process::exit(rc);
 }
